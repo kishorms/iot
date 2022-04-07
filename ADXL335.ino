aafebae8ee63793cdb7e03b2dd4f4ef6a8faff07
Kishor Sonawane <kishormsonawane@gmail.com>
2022-04-07 17:41:46 +0530

    Create ADXL335.ino
    Working code for ADXL335

---
+++ ADXL335.ino
@@ -0,0 +1,84 @@
+
+#include <math.h>
+const int x_out = A1; /* connect x_out of module to A1 of UNO board */
+const int y_out = A2; /* connect y_out of module to A2 of UNO board */
+const int z_out = A3; /* connect z_out of module to A3 of UNO board */
+
+/*
+ * Ref : https://www.electronicwings.com/arduino/adxl335-accelerometer-interfacing-with-arduino-uno#:~:text=ADXL335%20Accelerometer%20Module&text=It%20can%20be%20used%20in,to%20digital%20signals%20using%20ADC.
+ */
+
+
+
+int x_adc_value, y_adc_value, z_adc_value; 
+ double roll, pitch, yaw;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(2000);
+  readxyz();
+
+}
+
+
+/* ADXL335 PIN Config
+ * VCC --->5V
+ * GND--->GND
+ * X  ---> A1
+ * Y  ---> A2
+ * Z  ---> A3
+ * 
+ */
+
+
+void readxyz()
+{
+
+double x_g_value, y_g_value, z_g_value;
+ 
+
+  
+   x_adc_value = analogRead(x_out); /* Digital value of voltage on x_out pin */ 
+  y_adc_value = analogRead(y_out); /* Digital value of voltage on y_out pin */ 
+  z_adc_value = analogRead(z_out); /* Digital value of voltage on z_out pin */ 
+  
+  Serial.println("test");
+  
+  Serial.print("x = ");
+  Serial.print(x_adc_value);
+  Serial.print("\t\t");
+  Serial.print("y = ");
+  Serial.print(y_adc_value);
+  Serial.print("\t\t");
+  Serial.print("z = ");
+  Serial.print(z_adc_value);
+  Serial.print("\t\t");
+//---- calculation
+
+//delay(100);
+  
+  x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in x-direction in g units */ 
+  y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in y-direction in g units */ 
+  z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); /* Acceleration in z-direction in g units */ 
+
+  roll = ( ( (atan2(y_g_value,z_g_value) * 180) / 3.14 ) + 180 ); /* Formula for roll */
+  pitch = ( ( (atan2(z_g_value,x_g_value) * 180) / 3.14 ) + 180 ); /* Formula for pitch */
+  //yaw = ( ( (atan2(x_g_value,y_g_value) * 180) / 3.14 ) + 180 ); /* Formula for yaw */
+  /* Not possible to measure yaw using accelerometer. Gyroscope must be used if yaw is also required */
+
+  Serial.print("Roll = ");
+  Serial.print(roll);
+  Serial.print("\t");
+  Serial.print("Pitch = ");
+  Serial.print(pitch);
+  Serial.print("\n\n");
+  delay(1000);
+
+  
+}
